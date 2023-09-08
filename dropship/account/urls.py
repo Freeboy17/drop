9fff243ed0b6a641b72acbdcc9c748e7983e20a5
freeboy <mikituykt24@gmail.com>
2023-09-08 20:17:43 +0300

    added login page and functionality, piece of cake(i stole it as well and have no freaking idea how it works)

--- dropship/account/urls.py
+++ dropship/account/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
 
-    path("register/", views.register_request, name="register")
+    path("register/", views.register_request, name="register"),
+    path("login/", views.login_request, name="login")
 ]
